0d43f06a911faf208ab45ab18d7d69f321e20633
Douglas Doner <ddoner01@seattlecentral.edu>
2015-05-19 17:55:37 -0700

    Adds custom_config file

---
+++ application/config/custom_config.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Custom_config.php
+ * Config file for hooking up theme
+ */
+
+ $config['style'] = 'flatly.css';
+ $config['banner'] = 'Default Banner';
+ $config['title'] = 'Default Title';
+ $config['copyright'] = 'Default Copyright';
+ $config['theme'] = 'themes/bootswatch/';
+ $config['banner-href'] = './';
