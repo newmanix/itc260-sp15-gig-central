8c982fdff541035bbd030d06b6b278d5a260dcb3
Douglas Doner <ddoner01@seattlecentral.edu>
2015-05-19 18:13:55 -0700

    Adds bootswatch theme header and footer

---
+++ application/views/themes/bootswatch/footer.php
@@ -0,0 +1,24 @@
+      <footer>
+        <div class="row">
+          <div class="col-lg-12">
+            
+            <ul class="list-unstyled">
+              <li class="pull-right"><a href="#top">Back to top</a></li>
+            </ul>
+			<p><?=$this->config->item('copyright')?>. Theme by <a href="http://bootswatch.com/">Bootswatch</a>, based on <a href="http://getbootstrap.com/css/">Bootstrap</a>.
+			</p>
+
+          </div>
+        </div>
+        
+      </footer>
+    
+
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="<?=base_url();?>public/themes/bootswatch/js/bootstrap.min.js"></script>
+    <script src="<?=base_url();?>public/themes/bootswatch/js/bootswatch.js"></script>
+  </body>
+</html>
