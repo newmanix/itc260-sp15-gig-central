11e111d813dd41927eb87731d54a4bc70cf11c94
naokity <naomiwatabetaylor@gmail.com>
2015-06-16 17:09:06 -0700

    Update Profile_form_model.php
    Add  the comment in between <?php
    
    class Profile_form_model extends CI_Model {

--- application/models/Profile_form_model.php
+++ application/models/Profile_form_model.php
@@ -1,5 +1,27 @@
 <?php
 
+/**
+ * models/Profile_form_model.php
+ * controller for a generic Profile_form
+ * used to show how to do CRUD in CodeIgniter
+ *
+ * @package ITC260
+ * @subpackage Profile_form
+ * @author
+ * @version 1.0 2015/5/14
+ * @link
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @see Profile_form.php
+ * @see index.php
+ * @todo none
+ */
+/**
+ * Profile_form model for our CRUD demo
+ *
+ * @see controllers/Profile_form.php
+ * @todo none
+ */
+ 
 class Profile_form_model extends CI_Model {
 
 	function __construct()
