4747fed98b49a2c941310689b9ab45b52e8dac63
nicole warnemuende <nmuende@gmail.com>
2015-08-06 18:47:28 -0700

    Updates application\views\gigs\view.php

--- application/views/gigs/view.php
+++ application/views/gigs/view.php
@@ -18,5 +18,17 @@
 
 <div class="container">
     <h2><?php echo $gig['CompanyName']; ?></h2>
-    <p><?php echo $gig['GigOutline']; ?></p>
+    <p><b>Company Address:</b><?php echo $gig['CompanyAddress']; ?></p>
+    <p><b>City: </b><?php echo $gig['City']; ?></p>
+    <p><b>State: </b><?php echo $gig['CompanyState']; ?></p>
+    <p><b>Website: </b><?php echo $gig['CompanyWebsite']; ?></p>
+    <p><b>Contact Name: </b><?php echo $gig['FirstName'] . " " . $gig['LastName']; ?></p>
+    <p><b>Email: </b><?php echo $gig['Email']; ?></p>
+    <p><b>Phone: </b><?php echo $gig['Phone']; ?></p>
+    <p><b>Employment Type: </b><?php echo $gig['EmploymentType']; ?></p>
+    <p><b>Gig information: </b><?php echo $gig['GigOutline']; ?></p>
+    <p><b>Special Instructions: </b><?php echo $gig['SpInstructions']; ?></p>
+    <p><b>Pay Rate: </b><?php echo $gig['PayRate']; ?></p>
+    <p><b>Date Posted: </b><?php echo $gig['GigPosted']; ?></p>
+    <p><b>Last Update: </b><?php echo $gig['LastUpdated']; ?></p>
 </div>
