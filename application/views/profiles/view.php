ce2ebfdb0f58f65457e7865cb23428a29fb9c3b2
souhaAmor <samor001@seattlecentral.edu>
2016-05-24 14:56:37 -0700

    update view.php

--- application/views/profiles/view.php
+++ application/views/profiles/view.php
@@ -3,7 +3,8 @@
 $this->load->view($this->config->item('theme') . 'header');
 
 echo '<h2>' . $profile['first_name'] . " " . $profile['last_name'] . '</h2>';
-echo $profile['email'];
+echo $profile['email'] ;
+echo '<br /> <h3> Bio </h3>'.$profile['bio'];
 echo '<p>' . anchor('profile/', 'Back to Profiles List') . '</p>';
 
 $this->load->view($this->config->item('theme') . 'footer');
