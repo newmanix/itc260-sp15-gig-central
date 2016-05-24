29c020fa99279c373cf0693a18a4022d8e440be8
rattana <rattana.neak@gmail.com>
2016-05-24 16:32:16 -0700

    make change back to the correct Profile

--- application/views/profiles/view.php
+++ application/views/profiles/view.php
@@ -3,8 +3,7 @@
 $this->load->view($this->config->item('theme') . 'header');
 
 echo '<h2>' . $profile['first_name'] . " " . $profile['last_name'] . '</h2>';
-echo $profile['email'] ;
-echo '<br /> <h3> Bio </h3>'.$profile['bio'];
+echo $profile['email'];
 echo '<p>' . anchor('profile/', 'Back to Profiles List') . '</p>';
 
 $this->load->view($this->config->item('theme') . 'footer');
