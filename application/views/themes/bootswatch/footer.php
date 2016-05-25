c2b2e81e6424ea6145d35f8c3681db80f2928f46
TheLoveHandle <adanie04@seattlecentral.edu>
2016-05-24 17:16:01 -0700

    Add files via upload

--- application/views/themes/bootswatch/footer.php
+++ application/views/themes/bootswatch/footer.php
@@ -1,28 +1,22 @@
-<footer>
+    <footer>
         <div class="row">
-		<div class="col-xs-12">
-            <?= $this->navigation->loadFooter(); ?>
-		</div>
           <div class="col-xs-12">
-		<hr />
-            <ul class="list-unstyled">
-              <li class="pull-right"><a href="#top">Back to top</a></li>
-            </ul>
-			<p><?=$this->config->item('copyright')?>. Theme by <a href="http://bootswatch.com/">Bootswatch</a>, based on <a href="http://getbootstrap.com/css/">Bootstrap</a>.
-			</p>
+		      <hr /><!-- this is the little white line that you see on the page -->
+                <ul class="list-unstyled">
+                <?= $this->navigation->loadFooter(); ?><!-- the actual nav items -->
+                <li class="pull-right"><a href="#top">Back to top</a></li>
+                </ul>
+			 <p><?=$this->config->item('copyright')?>. Theme by <a href="http://bootswatch.com/">Bootswatch</a>, based on <a href="http://getbootstrap.com/css/">Bootstrap</a>.</p>
 
           </div>
         </div>
-
-      </footer>
-
-
-    </div>
-
+    </footer>
+</div>
 
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <script src="<?=base_url();?>public/themes/bootswatch/js/bootstrap.min.js"></script>
     <script src="<?=base_url();?>public/themes/bootswatch/js/bootswatch.js"></script>
     <script src="http://beneposto.pl/jqueryrotate/js/jQueryRotateCompressed.js"></script>
+
   </body>
 </html>
