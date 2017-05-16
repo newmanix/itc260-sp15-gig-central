0d165c7a6ac2ff169aa2f4625de852252cc7dcc2
Gracie <grace@ugraphix.com>
2017-05-16 14:33:25 -0700

    Active Links
    Made company website links active (after you select ‘read more’ when viewing gigs)

--- application/views/gigs/view.php
+++ application/views/gigs/view.php
@@ -17,60 +17,22 @@
 ?>
 <?php $this->load->view($this->config->item('theme') . 'header'); ?>
 
-<div class="container list-group">
-    <div class="list-group-item">
-        <h2 class="list-group-item-heading">
-            <?php echo $gig['Name']; ?>
-        </h2>
-            <ul class="list-group-item-text">
-                <li>
-                    <?php echo "<b>Company Address:</b> " . $gig['Address'] . ", " . $gig['CompanyCity']  . ", " .  $gig['State']; ?>
-                </li>
-                <li>
-                    <b>Website: </b><?php echo $gig['Website']; ?>
-                </li>
-            </ul>
-    </div>
-    <div class="list-group-item">
-        <h2 class="list-group-item-heading">
-            Contact
-        </h2>
-            <ul class="list-group-item-text">
-                <li>
-                    <b>Contact Name: </b><?php echo $gig['FirstName'] . " " . $gig['LastName']; ?>
-                </li>
-                <li>
-                    <b>Email: </b><?php echo $gig['Email']; ?>
-                </li>
-                <li>
-                    <b>Phone: </b><?php echo $gig['Phone']; ?>
-                </li>
-            </ul>
-    </div>
-    <div class="list-group-item">    
-        <h2 class="list-group-item-heading">
-            Gig Details
-        </h2>
-            <ul class="list-group-item-text">
-                <li>
-                    <b>Employment Type: </b><?php echo $gig['EmploymentType']; ?>
-                </li>
-                <li>
-                    <b>Gig information: </b><?php echo $gig['GigOutline']; ?>
-                </li>
-                <li>
-                    <b>Special Instructions: </b><?php echo $gig['SpInstructions']; ?>
-                </li>
-                <li>
-                    <b>Pay Rate: </b><?php echo $gig['PayRate']; ?>
-                </li>
-                <!--<p><b>Date Posted: </b><?php //echo $gig['GigPosted']; ?><!--</p>-->
-                <li>
-                    <b>Date Posted: </b><?php echo date('Y-m-d H:i:s'); ?>
-                </li>
-                <!--<p><b>Last Update: </b><?php //echo $gig['LastUpdated']; ?><!--</p>-->
-            </ul>
-    </div>
+<div class="container">
+    <h2><?php echo $gig['Name']; ?></h2>
+    <p><b>Company Address:</b><?php echo $gig['Address']; ?></p>
+    <p><b>City: </b><?php echo $gig['CompanyCity']; ?></p>
+    <p><b>State: </b><?php echo $gig['State']; ?></p>
+    <p><b>Website: </b><a href="<?php echo $gig['Website']; ?>"><?php echo $gig['Website']; ?></a></p>
+    <p><b>Contact Name: </b><?php echo $gig['FirstName'] . " " . $gig['LastName']; ?></p>
+    <p><b>Email: </b><?php echo $gig['Email']; ?></p>
+    <p><b>Phone: </b><?php echo $gig['Phone']; ?></p>
+    <p><b>Employment Type: </b><?php echo $gig['EmploymentType']; ?></p>
+    <p><b>Gig information: </b><?php echo $gig['GigOutline']; ?></p>
+    <p><b>Special Instructions: </b><?php echo $gig['SpInstructions']; ?></p>
+    <p><b>Pay Rate: </b><?php echo $gig['PayRate']; ?></p>
+    <!--<p><b>Date Posted: </b><?php //echo $gig['GigPosted']; ?></p>-->
+    <p><b>Date Posted: </b><?php echo date('Y-m-d H:i:s'); ?></p>
+    <!--<p><b>Last Update: </b><?php //echo $gig['LastUpdated']; ?></p>-->
 </div>
 
 <?php $this->load->view($this->config->item('theme') . 'footer'); ?>
